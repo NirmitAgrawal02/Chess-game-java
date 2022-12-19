2215f86d2d6795500925c7dc9594fe816dc08c1e
Nirmit Agrawal <nagraw18@asu.edu>
2022-12-18 20:57:12 -0700

    Pieces Types

---
+++ src/Pieces/pieces.java
@@ -0,0 +1,16 @@
+package Pieces;
+import Game.*;
+
+/**
+ * Author - Nirmit Agrawal
+ * Version - 1.0
+ */
+public abstract class pieces {
+
+    public pieces()
+    {
+
+    }
+    public abstract boolean isValidPath();
+    public abstract int move();
+}
